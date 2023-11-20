119aa26ece12cb8f5f25103af02fc65aafd319f6
Jingru Xu <nicoxu1000@gmail.com>
2023-11-20 01:00:33 -0500

    Add new repo to deploy

---
+++ build/script.js
@@ -0,0 +1,98 @@
+// Create a single instance of Calculator
+const calculator = new Calculator();
+
+// Capture user input
+const buttons = document.querySelector("#calculator-buttons");
+buttons.addEventListener("click", function (event) {
+  // If clicked other area besides buttons, return
+  if (event.target.type !== "button") return;
+
+  // Call the calculateInput method on the existing calculator instance
+  calculator.calculateInput(event.target.value);
+});
+
+function Calculator() {
+  this.display = "0";
+  this.previousTotal = null;
+
+  this.calculateInput = function (value) {
+    // Check if the user clicked special buttons
+    switch (value) {
+      case "AC":
+        // Clear the screen
+        this.display = "0";
+        this.previousTotal = null;
+        this.showOnScreen(this.display);
+        break;
+      case "=":
+        // Calculate the answer
+        this.getAnswer(this.display);
+        break;
+
+      case ".":
+        // Add a decimal point
+        const lastChar = this.display.slice(-1);
+
+        // Allow adding a decimal point only if the last character is a number
+        if (!isNaN(lastChar)) {
+          // Find the position of the last operator (+, -, *, /)
+          const lastOperatorIndex = Math.max(
+            this.display.lastIndexOf("+"),
+            this.display.lastIndexOf("-"),
+            this.display.lastIndexOf("*"),
+            this.display.lastIndexOf("/")
+          );
+
+          // Get the current number, slice from the position after the last operator.
+          const currentNumber = this.display.slice(lastOperatorIndex + 1);
+
+          // Check if the current number already contains a decimal point
+          if (!currentNumber.includes(".")) {
+            this.addToDisplay(value);
+          }
+        }
+        break;
+      default:
+        // Add a number to the display
+        this.addToDisplay(value);
+    }
+  };
+
+  this.addToDisplay = function (value) {
+    if (this.display === "0") {
+      this.display = "";
+    } else if (this.previousTotal !== null) {
+      this.display = this.previousTotal;
+      this.previousTotal = null;
+    }
+
+    // Check if user enters an invalid combination of operations
+    const lastChar = this.display.slice(-1);
+    if (isNaN(lastChar) && isNaN(value)) return;
+
+    this.display += value;
+    this.showOnScreen(this.display);
+  };
+
+  this.showOnScreen = function (display) {
+    const screen = document.querySelector("#display-screen");
+    screen.value = display;
+  };
+
+  this.getAnswer = function (display) {
+    let result = Function(`return ${display}`)();
+    result = parseFloat(result.toFixed(9));
+
+    // give error if divide by zero
+    if (result === Infinity) {
+      result = "Error";
+    }
+    // limit to 9 digits if answer is too long
+
+    // Check if the result exceeds the length of the display
+    if (result.toString().length > 10) {
+      result = parseFloat(result.toPrecision(10));
+    }
+    this.showOnScreen(result);
+  };
+}
